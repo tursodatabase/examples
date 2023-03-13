3c0c52d38a82af7cff9bf72468fa96e129fc0d1a
James Sinkala <xinnks@gmail.com>
2023-03-13 18:11:50 +0300

    feat: Add helper functions

---
+++ src/routes/utils.ts
@@ -0,0 +1,26 @@
+import { type ResultSet, type SqlValue } from "@libsql/client";
+
+export function responseDataAdapter(data: ResultSet): any[] {
+    if (!data?.columns || !data?.rows) {
+      throw new Error("Malformed response from turso");
+    }
+  
+    const { columns, rows } = data;
+    const formattedData: any[] = [];
+  
+    for (const row of rows) {
+      const rowData: { [k: string]: SqlValue } = {};
+      for (let i = 0; i < columns.length; i++) {
+        rowData[columns[i]] = row[i];
+      }
+  
+      // This line of code is assuming that all of the fields of
+      // data returned are present in the columns and rows of the
+      // ResultSet, and their types match. Ideally, this code checks the presence
+      // and validity of each value in the ResultSet before passing it along to
+      // the caller.
+      formattedData.push(rowData as unknown as any);
+    }
+  
+    return formattedData;
+  }
