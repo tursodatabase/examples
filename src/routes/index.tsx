0e41142b798a85f428535953d525ae469ab689b0
James Sinkala <xinnks@gmail.com>
2023-03-18 17:45:06 +0300

    chore: lint and format code

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,66 +1,76 @@
-import { component$ } from '@builder.io/qwik';
-import {
-  type DocumentHead,
-  globalAction$,
-  Form,
-} from '@builder.io/qwik-city';
+import { component$ } from "@builder.io/qwik";
+import { type DocumentHead, globalAction$, Form } from "@builder.io/qwik-city";
 import { createClient } from "@libsql/client/http";
-import { LoadingAnimation } from '~/components/loading/loading';
-import { Noty } from '~/components/notification/notification';
-import { responseDataAdapter } from './utils';
+import { LoadingAnimation } from "~/components/loading/loading";
+import { Noty } from "~/components/notification/notification";
+import { responseDataAdapter } from "./utils";
 
 export const BASE_URL = import.meta.env.VITE_BASE_URL;
 
-export const useFormAction = globalAction$(async(form) => {
+export const useFormAction = globalAction$(async (form) => {
   const db = createClient({
-    url: import.meta.env.VITE_DB_URL
+    url: import.meta.env.VITE_DB_URL,
   });
 
   // check if atleast one link was added
-  if(
-    !(form.twitter as string).includes('twitter.com/') &&
-    !(form.linkedin as string).includes('linkedin.com/') &&
-    !(form.facebook as string).includes('facebook.com/') &&
-    !(form.github as string).includes('github.com/') &&
-    !(form.youtube as string).includes('youtube.com/')
+  if (
+    !(form.twitter as string).includes("twitter.com/") &&
+    !(form.linkedin as string).includes("linkedin.com/") &&
+    !(form.facebook as string).includes("facebook.com/") &&
+    !(form.github as string).includes("github.com/") &&
+    !(form.youtube as string).includes("youtube.com/")
   ) {
     return {
       success: false,
-      message: "Add at least one link!"
-    }
+      message: "Add at least one link!",
+    };
   }
 
-  // add account 
+  // add account
   await db.execute(
     "insert into users(email, username, full_name) values(?, ?, ?)",
-    [(form.email as string), (form.username as string), (form.fullname as string)]
+    [form.email as string, form.username as string, form.fullname as string]
   );
 
-  const response = await db.execute(
-    "select * from users where email = ?",
-    [(form.email as string)]
-  );
+  const response = await db.execute("select * from users where email = ?", [
+    form.email as string,
+  ]);
   const userData = responseDataAdapter(response);
-  const {id, username} = userData[0]
+  const { id, username } = userData[0];
 
   // add links
-  if((form.twitter as string).includes('twitter.com/'))
-    await db.execute("insert into links(user_id, website, link) values(?, ?, ?)",[id, "Twitter", form.twitter]);
-  if((form.linkedin as string).includes('linkedin.com/'))
-    await db.execute("insert into links(user_id, website, link) values(?, ?, ?)",[id, "Linkedin", form.linkedin]);
-  if((form.facebook as string).includes('facebook.com/'))
-    await db.execute("insert into links(user_id, website, link) values(?, ?, ?)",[id, "Facebook", form.facebook]);
-  if((form.github as string).includes('github.com/'))
-    await db.execute("insert into links(user_id, website, link) values(?, ?, ?)",[id, "GitHub", form.github]);
-  if((form.youtube as string).includes('youtube.com/'))
-    await db.execute("insert into links(user_id, website, link) values(?, ?, ?)",[id, "Youtube", form.youtube]);
+  if ((form.twitter as string).includes("twitter.com/"))
+    await db.execute(
+      "insert into links(user_id, website, link) values(?, ?, ?)",
+      [id, "Twitter", form.twitter]
+    );
+  if ((form.linkedin as string).includes("linkedin.com/"))
+    await db.execute(
+      "insert into links(user_id, website, link) values(?, ?, ?)",
+      [id, "Linkedin", form.linkedin]
+    );
+  if ((form.facebook as string).includes("facebook.com/"))
+    await db.execute(
+      "insert into links(user_id, website, link) values(?, ?, ?)",
+      [id, "Facebook", form.facebook]
+    );
+  if ((form.github as string).includes("github.com/"))
+    await db.execute(
+      "insert into links(user_id, website, link) values(?, ?, ?)",
+      [id, "GitHub", form.github]
+    );
+  if ((form.youtube as string).includes("youtube.com/"))
+    await db.execute(
+      "insert into links(user_id, website, link) values(?, ?, ?)",
+      [id, "Youtube", form.youtube]
+    );
 
   return {
     success: true,
     message: "Links added!",
-    username: username
-  }
-})
+    username: username,
+  };
+});
 
 export default component$(() => {
   const formAction = useFormAction();
@@ -70,80 +80,85 @@ export default component$(() => {
       <h1 class="p-2 text-center font-bold text-xl text-teal-700">Mylinks</h1>
 
       <div>
-        {
-          formAction.isRunning && <div class="flex justify-center w-full"><LoadingAnimation/></div>
-        }
-        {
-          formAction.value?.success && <div class="flex flex-col justify-center space-y-2">
-            <Noty message={formAction.value?.message || "Links added!"} type="success"></Noty>
+        {formAction.isRunning && (
+          <div class="flex justify-center w-full">
+            <LoadingAnimation />
+          </div>
+        )}
+        {formAction.value?.success && (
+          <div class="flex flex-col justify-center space-y-2">
+            <Noty
+              message={formAction.value?.message || "Links added!"}
+              type="success"
+            ></Noty>
             <p class="p-2 text-center">
-              You social links are now available at <a href={`${BASE_URL}/u/${formAction.value?.username}`} target="_blank">{`${BASE_URL}/u/${formAction.value?.username}`}</a>
+              You social links are now available at{" "}
+              <a
+                href={`${BASE_URL}/u/${formAction.value?.username}`}
+                target="_blank"
+              >{`${BASE_URL}/u/${formAction.value?.username}`}</a>
             </p>
           </div>
-        }
-        {
-          formAction.formData && !formAction.isRunning && !formAction.value?.success && <Noty message={formAction.value?.message || "Failed to add you links!"} type="error"></Noty>
-        }
+        )}
+        {formAction.formData &&
+          !formAction.isRunning &&
+          !formAction.value?.success && (
+            <Noty
+              message={formAction.value?.message || "Failed to add you links!"}
+              type="error"
+            ></Noty>
+          )}
       </div>
 
-      <Form
-        class="p-4 flex flex-col space-y-2"
-        action={formAction}
-        spaReset
-      >
-        <div class="p-2 font-semibold text-teal-600">
-          User Details
+      <Form class="p-4 flex flex-col space-y-2" action={formAction} spaReset>
+        <div class="p-2 font-semibold text-teal-600">User Details</div>
+        <div>
+          <input type="text" name="fullname" placeholder="Full Name" required />
         </div>
         <div>
-          <input type="text" name="fullname" placeholder="Full Name" required
-            />
+          <input
+            type="email"
+            name="email"
+            placeholder="someone@mail.com"
+            required
+          />
         </div>
         <div>
-          <input type="email" name="email" placeholder="someone@mail.com" required
-            />
+          <input type="text" name="username" placeholder="username" required />
+        </div>
+        <div class="p-2 font-semibold text-teal-600">Social Links</div>
+        <div>
+          <input type="text" name="twitter" placeholder="Twitter" />
         </div>
         <div>
-          <input type="text" name="username" placeholder="username" required
-            />
-        </div>
-        <div class="p-2 font-semibold text-teal-600">
-          Social Links
+          <input type="text" name="linkedin" placeholder="Linkedin" />
         </div>
         <div>
-          <input type="text" name="twitter" placeholder="Twitter"
-            />
+          <input type="text" name="facebook" placeholder="Facebook" />
         </div>
         <div>
-          <input type="text" name="linkedin" placeholder="Linkedin"
-            />
+          <input type="text" name="github" placeholder="GitHub" />
         </div>
         <div>
-          <input type="text" name="facebook" placeholder="Facebook"
-            />
-        </div>
-        <div>
-          <input type="text" name="github" placeholder="GitHub"
-            />
-        </div>
-        <div>
-          <input type="text" name="youtube" placeholder="Youtube"
-            />
+          <input type="text" name="youtube" placeholder="Youtube" />
         </div>
         <div class="pt-2">
           <button type="submit">Add Links</button>
         </div>
       </Form>
-      <p class="text-center text-gray-400 italic">~ This little app works even when JavaScript is disabled. ~</p>
+      <p class="text-center text-gray-400 italic">
+        ~ This little app works even when JavaScript is disabled. ~
+      </p>
     </div>
   );
 });
 
 export const head: DocumentHead = {
-  title: 'Mylinks',
+  title: "Mylinks",
   meta: [
     {
       name: "description",
-      content: "All your social links in one place."
-    }
-  ]
+      content: "All your social links in one place.",
+    },
+  ],
 };
--- src/routes/u/[username]/index.tsx
+++ src/routes/u/[username]/index.tsx
@@ -1,16 +1,13 @@
-import { component$ } from '@builder.io/qwik';
-import {
-  type DocumentHead,
-  routeLoader$,
-} from '@builder.io/qwik-city';
+import { component$ } from "@builder.io/qwik";
+import { type DocumentHead, routeLoader$ } from "@builder.io/qwik-city";
 import { createClient } from "@libsql/client/http";
-import { Facebook } from '~/components/icons/facebook';
-import { GitHub } from '~/components/icons/github';
-import { LinkedIn } from '~/components/icons/linkedin';
-import { Twitter } from '~/components/icons/twitter';
-import { User } from '~/components/icons/user';
-import { Youtube } from '~/components/icons/youtube';
-import { responseDataAdapter } from '~/routes/utils';
+import { Facebook } from "~/components/icons/facebook";
+import { GitHub } from "~/components/icons/github";
+import { LinkedIn } from "~/components/icons/linkedin";
+import { Twitter } from "~/components/icons/twitter";
+import { User } from "~/components/icons/user";
+import { Youtube } from "~/components/icons/youtube";
+import { responseDataAdapter } from "~/routes/utils";
 
 interface SocialLinks {
   website: string;
@@ -28,89 +25,100 @@ interface UserLinksResponse {
   links: SocialLinks[] | null;
 }
 
-export const ICON_COLOR_STYLE="fill-teal-700";
+export const ICON_COLOR_STYLE = "fill-teal-700";
 
 export const SocialLink = (props: SocialLinks) => {
   const getIcon = () => {
-    if(props.website === "GitHub") return <GitHub colorStyle={ICON_COLOR_STYLE}/>
-    if(props.website === "Twitter") return <Twitter colorStyle={ICON_COLOR_STYLE}/>
-    if(props.website === "Youtube") return <Youtube colorStyle={ICON_COLOR_STYLE}/>
-    if(props.website === "Linkedin") return <LinkedIn colorStyle={ICON_COLOR_STYLE}/>
-    if(props.website === "Facebook") return <Facebook colorStyle={ICON_COLOR_STYLE} />
-  }
+    if (props.website === "GitHub")
+      return <GitHub colorStyle={ICON_COLOR_STYLE} />;
+    if (props.website === "Twitter")
+      return <Twitter colorStyle={ICON_COLOR_STYLE} />;
+    if (props.website === "Youtube")
+      return <Youtube colorStyle={ICON_COLOR_STYLE} />;
+    if (props.website === "Linkedin")
+      return <LinkedIn colorStyle={ICON_COLOR_STYLE} />;
+    if (props.website === "Facebook")
+      return <Facebook colorStyle={ICON_COLOR_STYLE} />;
+  };
   const url = () => {
-    if(!props.link.includes("http")){
-      return "https://" + props.link.replace(/^(http(s)?:\/\/)/gi,"https://")
+    if (!props.link.includes("http")) {
+      return "https://" + props.link.replace(/^(http(s)?:\/\/)/gi, "https://");
     }
     return props.link;
-  }
+  };
   return (
-    <a
-      class="flex space-x-3"
-      href={url()}
-      target="_blank"
-    >
-        <div id="social-link" class="w-full p-2 text-gray-600 bg-teal-200 hover:bg-teal-300 rounded-md flex space-x-3">{ getIcon() } <span>{ props.website }</span></div>
+    <a class="flex space-x-3" href={url()} target="_blank">
+      <div
+        id="social-link"
+        class="w-full p-2 text-gray-600 bg-teal-200 hover:bg-teal-300 rounded-md flex space-x-3"
+      >
+        {getIcon()} <span>{props.website}</span>
+      </div>
     </a>
-  )
-}
-
-export const useLinksLoader = routeLoader$(async({params, status}): Promise<UserLinksResponse> => {
-  const db = createClient({
-    url: import.meta.env.VITE_DB_URL
-  });
-  const userResponse = await db.execute(
-    "select full_name, username, id from users where username = ?;",
-    [params.username]
   );
-  const userData = responseDataAdapter(userResponse);
-  if(!userData.length){
-    status(404);
+};
+
+export const useLinksLoader = routeLoader$(
+  async ({ params, status }): Promise<UserLinksResponse> => {
+    const db = createClient({
+      url: import.meta.env.VITE_DB_URL,
+    });
+    const userResponse = await db.execute(
+      "select full_name, username, id from users where username = ?;",
+      [params.username]
+    );
+    const userData = responseDataAdapter(userResponse);
+    if (!userData.length) {
+      status(404);
+      return {
+        user: null,
+        links: null,
+      };
+    }
+
+    const { id } = userData[0];
+    const linksResponse = await db.execute(
+      "select website, link from links where user_id = ?;",
+      [id]
+    );
+    const linksData = responseDataAdapter(linksResponse);
     return {
-      user: null,
-      links: null
+      user: userData[0],
+      links: linksData,
     };
   }
-
-  const { id } = userData[0];
-  const linksResponse = await db.execute(
-    "select website, link from links where user_id = ?;",
-    [id]
-  );
-  const linksData = responseDataAdapter(linksResponse);
-  return {
-    user: userData[0],
-    links: linksData
-  };
-})
+);
 
 export default component$(() => {
   const links = useLinksLoader();
 
   if (!links.value.user) {
-    return <div class="flex flex-col justify-center items-center p-8">
-      <div class="text-2xl p-4 text-teal-700">User doesn't exist!</div>
-    </div>;
+    return (
+      <div class="flex flex-col justify-center items-center p-8">
+        <div class="text-2xl p-4 text-teal-700">User doesn't exist!</div>
+      </div>
+    );
   }
-  
+
   return (
     <div class="p-8 mx-auto max-w-xl">
-      <h1 class="p-2 text-center font-bold text-xl text-teal-700 flex justify-center space-x-3"> <User/> <span>{links.value.user.full_name}</span></h1>
-      <div
-        class="p-4 flex flex-col space-y-3"
-      >
-        {
-          links.value.links!.map((item: SocialLinks) => <div key={item.id}>
+      <h1 class="p-2 text-center font-bold text-xl text-teal-700 flex justify-center space-x-3">
+        {" "}
+        <User /> <span>{links.value.user.full_name}</span>
+      </h1>
+      <div class="p-4 flex flex-col space-y-3">
+        {links.value.links!.map((item: SocialLinks) => (
+          <div key={item.id}>
             <SocialLink {...item}></SocialLink>
-          </div>)
-        }
+          </div>
+        ))}
       </div>
     </div>
   );
 });
 
-export const head: DocumentHead = ({params}) => {
+export const head: DocumentHead = ({ params }) => {
   return {
-    title: params.username + ' - Mylinks'
-  }
+    title: params.username + " - Mylinks",
+  };
 };
