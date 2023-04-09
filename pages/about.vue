83f19dbcbbe13caa5ed9c73248407ff6673b7e4b
James Sinkala <xinnks@gmail.com>
2023-04-09 13:59:25 +0300

    improvement: Update about us page

--- pages/about.vue
+++ pages/about.vue
@@ -5,7 +5,14 @@ useHead({
 </script>
 
 <template>
-    <h1>
-        The About page
-    </h1>
+    
+    <h1> About Us </h1>
+    
+    <p class="text-center">
+        <span class="font-semibold">Top Web Frameworks</span> lists the top frameworks for web development based on their GitHub stars count.
+    </p>
+    
+    <p class="text-center">
+        Feel free to contribute to the list by <nuxt-link to="/add-new">making a new submission.</nuxt-link>
+    </p>
 </template>
