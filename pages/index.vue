da1aa8159bfe8c5ee89679e7633d12f22a8bdc5a
James Sinkala <xinnks@gmail.com>
2023-04-10 00:04:27 +0300

    feat: Get and display city geolocation details from the vercel-edge headers

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <script setup>
 const { data } = await useFetch('/api/frameworks');
-const { data: frameworks, message } = data.value;
+const { data: {frameworks, city}, message } = data.value;
 
 const formatNumber = (val) => new Intl.NumberFormat('en-US', { maximumSignificantDigits: 3 }).format(val);
 
@@ -9,6 +9,10 @@ const formatNumber = (val) => new Intl.NumberFormat('en-US', { maximumSignifican
 <template>
   
   <h1 class="text-2xl font-bold text-center">Top Web Frameworks</h1>
+
+  <p
+    class="text-center text-gray-400 italic"
+  >{{ city }}</p>
 
   <div class="overflow-x-auto rounded-lg border border-gray-200">
     <table class="min-w-full divide-y-2 divide-gray-200 text-sm">
--- server/api/frameworks.js
+++ server/api/frameworks.js
@@ -1,13 +1,18 @@
 import useTurso from "~~/composables/use-turso";
 
-export default defineEventHandler(async () => {
+export default defineEventHandler(async (event) => {
   const client = useTurso();
-  
   const result = await client.execute("select * from frameworks order by stars desc");
+  const cityHeader = getHeader(event, 'x-vercel-ip-city');
+  const city = cityHeader ? decodeURIComponent(cityHeader) : '-';
+
+  console.log({ city });
 
   return {
     message: "Frameworks fetched!",
-    data: responseDataAdapter(result)
+    data: {
+      frameworks: responseDataAdapter(result),
+      city
+    }
   };
-
 })
