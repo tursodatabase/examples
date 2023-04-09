ba53f4dfd0d49f1c94f60f1b075a778a061f6965
James Sinkala <xinnks@gmail.com>
2023-04-09 13:13:20 +0300

    improvement(/layout): Update navigation menu

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,10 +1,12 @@
 <template>
-  <main>
-    <nav>
-      <NuxtLink to="/">Home</NuxtLink> |
-      <NuxtLink to="/about">About</NuxtLink>
-    </nav>
-
+  <nav
+    class="py-2 px-4 flex space-x-2"
+  >
+    <NuxtLink to="/" class="text-blue-600 hover:text-blue-700 font-semibold">Home</NuxtLink>
+    <NuxtLink to="/add-new" class="text-blue-600 hover:text-blue-700 font-semibold">Add New</NuxtLink>
+    <NuxtLink to="/about" class="text-blue-600 hover:text-blue-700 font-semibold">About</NuxtLink>
+  </nav>
+  <main class="p-4 flex flex-col space-y-6">
     <slot/>
   </main>
 </template>
