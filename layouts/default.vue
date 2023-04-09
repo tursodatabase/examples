473c3af94334a0089b9073e743eff0d00afb0301
James Sinkala <xinnks@gmail.com>
2023-04-09 14:00:42 +0300

    improvement(/layout): Update default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,11 +2,15 @@
   <nav
     class="py-2 px-4 flex space-x-2"
   >
-    <NuxtLink to="/" class="text-blue-600 hover:text-blue-700 font-semibold">Home</NuxtLink>
-    <NuxtLink to="/add-new" class="text-blue-600 hover:text-blue-700 font-semibold">Add New</NuxtLink>
-    <NuxtLink to="/about" class="text-blue-600 hover:text-blue-700 font-semibold">About</NuxtLink>
+    <NuxtLink to="/" class="font-semibold">Home</NuxtLink>
+    <NuxtLink to="/add-new" class="font-semibold">Add New</NuxtLink>
+    <NuxtLink to="/about" class="font-semibold">About</NuxtLink>
   </nav>
   <main class="p-4 flex flex-col space-y-6">
     <slot/>
+
+    <div class="text-gray-400 text-center pt-6">
+      Built with <a href="https://nuxt.com">Nuxt</a> and <a href="https://turso.tech">Turso</a>.
+    </div>
   </main>
 </template>
