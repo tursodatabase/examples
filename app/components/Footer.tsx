5ec24b7d7d6d25f3567a6735ecda9c70d3f9e085
James Sinkala <xinnks@gmail.com>
2023-05-20 16:29:05 +0300

    chore: Relocate components

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image'
 
 export default function Footer() {
   return (
-    <div className="z-10 w-full max-w-5xl mx-auto items-center justify-center font-mono text-sm lg:flex">
+    <footer className="z-10 w-full max-w-5xl mx-auto items-center justify-center font-mono text-sm lg:flex">
       <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
         <a
           className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto"
@@ -14,13 +14,21 @@ export default function Footer() {
           <Image
             src="/turso.svg"
             alt="Turso Logo"
-            className="dark:invert"
+            className='block dark:hidden'
+            width={150}
+            height={36}
+            priority
+          />
+          <Image
+            src="/turso-light.svg"
+            alt="Turso Logo"
+            className='hidden dark:block'
             width={150}
             height={36}
             priority
           />
         </a>
       </div>
-    </div>
+    </footer>
   )
 }
