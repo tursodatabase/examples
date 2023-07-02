1fa38d239b5bbc4e3c61719a5fa18aec8bc90f8a
James Sinkala <xinnks@gmail.com>
2023-07-02 07:43:28 +0300

    chore: Update package.json, add test and local migration scripts; add local migration script file

---
+++ migrate/index.ts
@@ -0,0 +1,56 @@
+import { createClient } from "@libsql/client";
+
+const client = createClient({
+  url: process.env.NUXT_TURSO_DB_URL as string,
+  authToken: process.env.NUXT_TURSO_DB_AUTH_TOKEN as string,
+});
+
+async function main() {
+  await client.execute(
+    "create table if not exists frameworks (id integer primary key,name varchar (50) not null,language varchar (50) not null,url text not null,stars integer not null)"
+  );
+  console.log("Migrated db!");
+
+  const statements = [
+    "create table if not exists frameworks (id integer primary key,name varchar (50) not null,language varchar (50) not null,url text not null,stars integer not null)",
+    "delete from frameworks",
+    {
+      sql: "insert into frameworks(name, language, url, stars) values(?, ?, ?, ?)",
+      args: ["Vue.js", "JavaScript", "https://github.com/vuejs/vue", 2000],
+    },
+    {
+      sql: "insert into frameworks(name, language, url, stars) values(?, ?, ?, ?)",
+      args: ["React", "JavaScript", "https://github.com/facebook/react", 2000],
+    },
+    {
+      sql: "insert into frameworks(name, language, url, stars) values(?, ?, ?, ?)",
+      args: [
+        "Angular",
+        "TypeScript",
+        "https://github.com/angular/angular",
+        87400,
+      ],
+    },
+    {
+      sql: "insert into frameworks(name, language, url, stars) values(?, ?, ?, ?)",
+      args: ["ASP.NET Core", "C#", "https://github.com/dotnet/aspnetcore", 300],
+    },
+    {
+      sql: "insert into frameworks(name, language, url, stars) values(?, ?, ?, ?)",
+      args: [
+        "Express",
+        "JavaScript",
+        "https://github.com/expressjs/express",
+        500,
+      ],
+    },
+  ];
+
+  statements.forEach(async (statement) => {
+    await client.execute(statement);
+  });
+
+  console.log("Seeded db");
+}
+
+main().catch((err) => console.log);
