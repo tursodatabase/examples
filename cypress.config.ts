a93cd3390967c9515889aad9eaa4c51962e60086
James Sinkala <xinnks@gmail.com>
2023-07-02 08:11:15 +0300

    style: Move baseUrl to env variable

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    baseUrl: "http://localhost:3000",
+    baseUrl: process.env.LOCAL_SITE_URL,
     setupNodeEvents(_on, _config) {
       // implement node event listeners here
     },
