97950aa0ae4fff164d1ff99254834e064878c329
James Sinkala <xinnks@gmail.com>
2023-07-18 23:16:26 +0300

    feat: Update session utility functions - Change how environment variables are passed to code within file to comply with latest Remix-Cloudflare workers update. - Use relations in Drizzle queries

--- app/lib/session.server.ts
+++ app/lib/session.server.ts
@@ -1,19 +1,11 @@
-import bcrypt from "bcryptjs";
+import { passwords, users } from "./../../drizzle/schema";
 import { redirect } from "@remix-run/cloudflare";
+import bcrypt from "bcryptjs";
 import { v4 as uuidv4 } from "uuid";
-import { eq } from "drizzle-orm";
 
-import { cartItems, passwords, users } from "drizzle/schema";
-import {
-  commitSession,
-  commitTempSession,
-  destroyTempSession,
-  getSession,
-  getTempSession,
-} from "~/sessions";
-
-import { db } from "./client";
-import type { User } from "./types";
+import { cookieSessionCreation } from "~/sessions";
+import { buildDbClient } from "./client";
+import { User } from "./types";
 
 export interface RegistrationData {
   firstName: string;
@@ -25,47 +17,48 @@ export interface RegistrationData {
   avatar?: string | null;
 }
 
-export async function login(email: string, password: string) {
-  // check if email exists
-  const user: any = await db
-    .select()
-    .from(users)
-    .leftJoin(passwords, eq(users.id, passwords.userId))
-    .where(eq(users.email, email))
-    .get();
-
-  if (!user) return null;
-
-  const isValidPassword = bcrypt.compareSync(
-    password,
-    (user.passwords.hash as string) || ""
-  );
-
-  if (!isValidPassword) return null;
-
-  return user.users;
+export interface LoginCredentials {
+  email: string;
+  password: string;
 }
 
-export async function createUserSession(userId: string, redirectUrl: string) {
-  const session = await getSession();
-  session.set("userId", userId);
-  // console.log("SessionData:  [createUserSession fn]", JSON.stringify(session));
-  return redirect(redirectUrl, {
-    headers: {
-      "Set-Cookie": await commitSession(session),
+export async function login(
+  { email, password }: LoginCredentials,
+  serverContext: any
+) {
+  const db = buildDbClient(serverContext);
+  // check if email exists
+  const user = await db.query.users.findFirst({
+    where: (users, { eq }) => eq(users.email, email),
+    with: {
+      password: true,
     },
   });
+
+  if (user !== undefined) {
+    const isValidPassword = bcrypt.compareSync(
+      password,
+      (user.password.hash as string) || ""
+    );
+
+    return !isValidPassword ? null : user;
+  }
+
+  return null;
 }
 
-export async function register({
-  firstName,
-  lastName,
-  email,
-  password,
-  phone,
-  address,
-  avatar,
-}: RegistrationData) {
+export async function register(
+  {
+    firstName,
+    lastName,
+    email,
+    password,
+    phone,
+    address,
+    avatar,
+  }: RegistrationData,
+  serverContext: any
+) {
   // validate
   if (!firstName || !lastName || !email || !password || !phone) {
     return {
@@ -74,18 +67,17 @@ export async function register({
     };
   }
 
-  // check if email exists
-  const existingUser = await db
-    .select({ email: users.email })
-    .from(users)
-    .where(eq(users.email, email))
-    .get();
+  const db = buildDbClient(serverContext);
 
-  if (existingUser && existingUser.email) {
+  const existingUser = await db.query.users.findFirst({
+    where: (users, { eq }) => eq(users.email, email),
+  });
+
+  if (existingUser !== undefined) {
     return {
-      message: "An account with this email exists",
+      message: "An account with this email already exists",
       user: null,
-    }; // user with email exists
+    };
   }
 
   const newUuid = uuidv4();
@@ -100,16 +92,15 @@ export async function register({
     })
     .returning()
     .get();
-  const userRecord = await db
-    .select()
-    .from(users)
-    .where(eq(users.email, email))
-    .get();
 
-  if (userRecord) {
+  const userRecord = await db.query.users.findFirst({
+    where: (users, { eq }) => eq(users.email, email),
+  });
+
+  if (userRecord !== undefined) {
     // create password record
     const userPassword = await bcrypt.hash(password, 10);
-    const newPassword = await db
+    await db
       .insert(passwords)
       .values({ hash: userPassword, userId: userRecord.id })
       .returning()
@@ -125,13 +116,48 @@ export async function register({
   };
 }
 
-export async function requireUserId({
-  request,
-  redirectTo = new URL(request.url).pathname,
-}: {
-  request: Request;
-  redirectTo?: string;
-}) {
+export async function createUserSession(
+  userId: string,
+  redirectUrl: string,
+  serverContext: any
+) {
+  const { getSession, commitSession } = cookieSessionCreation(serverContext);
+  const session = await getSession();
+  session.set("userId", userId);
+
+  return redirect(redirectUrl, {
+    headers: {
+      "Set-Cookie": await commitSession(session),
+    },
+  });
+}
+
+export async function destroyUserSession(
+  userId: string,
+  redirectUrl: string,
+  serverContext: any
+) {
+  const { getSession, destroySession } = cookieSessionCreation(serverContext);
+  const session = await getSession();
+
+  return redirect(redirectUrl, {
+    headers: {
+      "Set-Cookie": await destroySession(session),
+    },
+  });
+}
+
+export async function requireUserId(
+  {
+    request,
+    redirectTo = new URL(request.url).pathname,
+  }: {
+    request: Request;
+    redirectTo?: string;
+  },
+  serverContext: any
+) {
+  const { getSession } = cookieSessionCreation(serverContext);
   const session = await getSession(request.headers.get("Cookie"));
   const userId = session.get("userId");
   if (!userId) {
@@ -139,50 +165,4 @@ export async function requireUserId({
     redirect(`/account/login?${searchParams}`);
   }
   return userId;
-}
-
-// this session gets deleted when a user authenticates
-export async function createUnknownUserSession(
-  userId: string = uuidv4(),
-  redirectUrl: string
-) {
-  const session = await getTempSession();
-  session.set("unknownUserId", userId);
-  return redirect(redirectUrl, {
-    headers: {
-      "Set-Cookie": await commitTempSession(session),
-    },
-  });
-}
-
-export async function transferCartItemsToKnownUser({
-  request,
-  redirectUrl,
-}: {
-  request: Request;
-  redirectUrl: string;
-}) {
-  const unknownUserSession = await getTempSession(
-    request.headers.get("Cookie")
-  );
-  const unknownUserId = unknownUserSession.get("unknownUserId");
-  if (unknownUserId) {
-    const session = await getSession(request.headers.get("Cookie"));
-    const userId = session.get("userId");
-    // update cart items with unknown user id
-    const reassignedCartItems = await db
-      .update(cartItems)
-      .set({ userId })
-      .where(eq(cartItems.userId, unknownUserId))
-      .returning()
-      .all();
-
-    // delete unknown user session
-    return redirect(redirectUrl, {
-      headers: {
-        "Set-Cookie": await destroyTempSession(unknownUserSession),
-      },
-    });
-  }
-  return true;
 }
