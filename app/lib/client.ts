21edf4b8893703dbfed68f04c7d5fadea1ce94f7
James Sinkala <xinnks@gmail.com>
2023-07-18 23:04:06 +0300

    refactor: Change how environment variables are passed to code within file based on latest Remix update.

--- app/lib/client.ts
+++ app/lib/client.ts
@@ -1,10 +1,24 @@
-import "dotenv/config";
 import { drizzle } from "drizzle-orm/libsql";
 import { createClient } from "@libsql/client/http";
+import * as schema from "../../drizzle/schema";
 
-export const client = createClient({
-  url: TURSO_DB_URL as string,
-  authToken: TURSO_DB_AUTH_TOKEN as string,
-});
+interface Env {
+  TURSO_DB_AUTH_TOKEN?: string;
+  TURSO_DB_URL?: string;
+}
 
-export const db = drizzle(client);
+export function buildDbClient(serverContext: any) {
+  const url = (serverContext.env as unknown as Env).TURSO_DB_URL?.trim();
+  if (url === undefined) {
+    throw new Error("TURSO_DB_URL is not defined");
+  }
+
+  const authToken = (
+    serverContext.env as unknown as Env
+  ).TURSO_DB_AUTH_TOKEN?.trim();
+  if (authToken === undefined) {
+    throw new Error("TURSO_DB_AUTH_TOKEN is not defined");
+  }
+
+  return drizzle(createClient({ url, authToken }), { schema });
+}
