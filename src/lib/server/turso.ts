305a39aae7e6a4db7d159e8b7d3916953affefa7
James Sinkala <xinnks@gmail.com>
2023-08-29 01:58:11 +0300

    chore: move Turso client to lib/server directory

--- src/lib/server/turso.ts
+++ src/lib/server/turso.ts
@@ -1,11 +1,11 @@
 import { type LibSQLDatabase, drizzle } from 'drizzle-orm/libsql';
 import { createClient } from '@libsql/client/web';
-import * as schema from '../../drizzle/schema';
+import * as schema from '../../../drizzle/schema';
 
 export function tursoClient(): LibSQLDatabase<typeof schema> {
 	const url = import.meta.env.VITE_TURSO_DB_URL?.trim();
 	if (url === undefined) {
-		throw new Error(`'VITE_TURSO_DB_URL is not defined' ${JSON.stringify(import.meta)}`);
+		throw new Error('VITE_TURSO_DB_URL is not defined');
 	}
 
 	const authToken = import.meta.env.VITE_TURSO_DB_AUTH_TOKEN?.trim();
