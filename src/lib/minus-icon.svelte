0a984caa42511a1a2f4a52106c81c06ab581c1b5
James Sinkala <xinnks@gmail.com>
2023-08-29 02:01:51 +0300

    chore: remove unnecessary components

--- src/lib/minus-icon.svelte
+++
@@ -1,7 +0,0 @@
-<script lang="ts">
-	export let styles: string = 'w-5 h-5';
-</script>
-
-<svg class={styles} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"
-	><path d="M5 11V13H19V11H5Z" /></svg
->
