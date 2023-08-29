27f9273b47b0d0a26e754ea04ab819b176b1af5a
James Sinkala <xinnks@gmail.com>
2023-08-29 03:46:46 +0300

    chore: Fix file indentation

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,11 +3,11 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 export default {
-	schema: './drizzle/schema.ts',
-	out: './drizzle/migrations',
-	driver: 'turso',
-	dbCredentials: {
-		url: process.env.VITE_TURSO_DB_URL as string,
-		authToken: process.env.VITE_TURSO_DB_AUTH_TOKEN as string
-	}
+  schema: './drizzle/schema.ts',
+  out: './drizzle/migrations',
+  driver: 'turso',
+  dbCredentials: {
+    url: process.env.VITE_TURSO_DB_URL as string,
+    authToken: process.env.VITE_TURSO_DB_AUTH_TOKEN as string
+  }
 } satisfies Config;
