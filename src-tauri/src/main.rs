395ac489fd08e99bfc3f1c91d7a27eab45218f7f
ad hoc <postma.marin@protonmail.com>
2023-10-27 19:15:09 +0200

    fix compile errors

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -6,13 +6,28 @@ use libsql::{params, Database};
 use serde::{Deserialize, Serialize};
 use std::env;
 
-use std::ptr::null;
 use std::time::SystemTime;
 use std::time::UNIX_EPOCH;
-use std::vec;
 
 use tracing::info;
 use uuid::Uuid;
+
+#[derive(Serialize, Debug)]
+struct Error {
+    msg: String,
+}
+
+type Result<T> = std::result::Result<T, Error>;
+
+impl<T> From<T> for Error
+    where T: std::error::Error,
+{
+    fn from(value: T) -> Self {
+        Self {
+            msg: value.to_string(),
+        }
+    }
+}
 
 #[derive(Deserialize, Serialize, Debug)]
 pub struct NoteItem {
@@ -24,9 +39,7 @@ pub struct NoteItem {
 }
 
 #[tauri::command]
-async fn get_all_notes() -> Result<Vec<NoteItem>, String> {
-    tracing_subscriber::fmt::init();
-
+async fn get_all_notes() -> Result<Vec<NoteItem>> {
     dotenv().expect(".env file not found");
 
     let db_path = env::var("DB_PATH").unwrap();
@@ -38,32 +51,26 @@ async fn get_all_notes() -> Result<Vec<NoteItem>, String> {
         db_path, auth_token, sync_url
     );
 
-    let db = match Database::open_with_remote_sync(db_path, sync_url, auth_token).await {
-        Ok(db) => db,
-        Err(error) => {
-            eprintln!("Error connecting to remote sync server: {}", error);
-            return Err(error.to_string());
-        }
-    };
+    let db = Database::open_with_remote_sync(db_path, sync_url, auth_token).await?;
 
-    let conn = db.connect().unwrap();
+    let conn = db.connect()?;
 
     print!("Syncing with remote database...");
     db.sync().await.unwrap();
     println!(" done");
 
-    let mut results = conn.query("SELECT * FROM notes", ()).await.unwrap();
+    let mut results = conn.query("SELECT * FROM notes", ()).await?;
 
     println!("Note entries:");
 
     let mut notes: Vec<NoteItem> = Vec::new();
-    while let Some(row) = results.next().unwrap() {
+    while let Some(row) = results.next()? {
         let note: NoteItem = NoteItem {
-            id: row.get(0).unwrap(),
-            title: row.get(1).unwrap(),
-            text: row.get(2).unwrap(),
-            created_at: row.get(3).unwrap(),
-            updated_at: row.get(4).unwrap(),
+            id: row.get(0)?,
+            title: row.get(1)?,
+            text: row.get(2)?,
+            created_at: row.get(3)?,
+            updated_at: row.get(4)?,
         };
         info!("  {:?}", note);
         notes.push(note);
@@ -74,125 +81,101 @@ async fn get_all_notes() -> Result<Vec<NoteItem>, String> {
 }
 
 #[tauri::command]
-async fn new_note() -> Result<NoteItem, String> {
-    tracing_subscriber::fmt::init();
+async fn new_note() -> Result<Option<NoteItem>> {
+        dotenv().expect(".env file not found");
 
-    dotenv().expect(".env file not found");
+        let db_path = env::var("DB_PATH").unwrap();
+        let sync_url = env::var("TURSO_SYNC_URL").unwrap();
+        let auth_token = env::var("TURSO_TOKEN").unwrap();
 
-    let db_path = env::var("DB_PATH").unwrap();
-    let sync_url = env::var("TURSO_SYNC_URL").unwrap();
-    let auth_token = env::var("TURSO_TOKEN").unwrap();
+        println!(
+            "Here are the env vars: _db_path: {:?}, _auth_token: {:?}, _sync_url: {:?}",
+            db_path, auth_token, sync_url
+        );
 
-    println!(
-        "Here are the env vars: _db_path: {:?}, _auth_token: {:?}, _sync_url: {:?}",
-        db_path, auth_token, sync_url
-    );
+        let db = Database::open_with_remote_sync(db_path, sync_url, auth_token).await?;
 
-    let db = match Database::open_with_remote_sync(db_path, sync_url, auth_token).await {
-        Ok(db) => db,
-        Err(error) => {
-            eprintln!("Error connecting to remote sync server: {}", error);
-            return Err(error.to_string());
-        }
-    };
+        let conn = db.connect()?;
 
-    let conn = db.connect().unwrap();
-
-    let id = Uuid::new_v4();
-    let created_at = match SystemTime::now().duration_since(UNIX_EPOCH) {
-        Ok(n) => n.as_secs(),
-        Err(_) => panic!("SystemTime before UNIX EPOCH!"),
-    };
-    let title = String::from("New note..");
-
-    let params = params![id.to_string(), title, created_at];
-
-    let mut response = conn
-        .query(
-            "INSERT INTO note (id, title, created_at) VALUES (?, ?, ?)",
-            params,
-        )
-        .await
-        .unwrap();
-
-    print!("Syncing with remote database...");
-    db.sync().await.unwrap();
-    println!(" done");
-
-    let mut added_note = NoteItem {
-        id: "".to_string(),
-        title: "".to_string(),
-        text: "".to_string(),
-        created_at: created_at,
-        updated_at: created_at,
-    };
-    if let Some(row) = response.next().unwrap() {
-        added_note = NoteItem {
-            id: row.get(0).unwrap(),
-            title: row.get(1).unwrap(),
-            text: row.get(2).unwrap(),
-            created_at: row.get(3).unwrap(),
-            updated_at: row.get(4).unwrap(),
+        let id = Uuid::new_v4();
+        let created_at = match SystemTime::now().duration_since(UNIX_EPOCH) {
+            Ok(n) => n.as_secs(),
+            Err(_) => panic!("SystemTime before UNIX EPOCH!"),
         };
-    }
+        let title = String::from("New note..");
 
-    print!("Syncing with remote database...");
-    db.sync().await.unwrap();
-    println!(" done");
+        let params = params![id.to_string(), title, created_at];
 
-    format!("New note: {:?}", added_note);
+        let mut response = conn
+            .query(
+                "INSERT INTO notes (id, title, created_at) VALUES (?, ?, ?)",
+                params,
+            )
+            .await?;
 
-    Ok(added_note)
+        print!("Syncing with remote database...");
+        db.sync().await?;
+        println!(" done");
+
+        let ret = match response.next()? {
+            Some(row) => {
+                Some(NoteItem {
+                    id: row.get(0).unwrap(),
+                    title: row.get(1).unwrap(),
+                    text: row.get(2).unwrap(),
+                    created_at: row.get(3).unwrap(),
+                    updated_at: row.get(4).unwrap(),
+                })
+            }
+            None => None,
+        };
+
+        print!("Syncing with remote database...");
+        db.sync().await?;
+        println!(" done");
+
+        format!("New note: {ret:?}");
+
+        Ok(ret)
 }
 
 #[tauri::command]
-async fn update_note(id: String, new_text: String) -> Result<NoteItem, String> {
-    tracing_subscriber::fmt::init();
+async fn update_note(id: String, new_text: String) -> Result<NoteItem> {
+        info!(id, new_text);
 
-    info!(id, new_text);
+        dotenv().expect(".env file not found");
 
-    dotenv().expect(".env file not found");
+        let db_path = env::var("DB_PATH").unwrap();
+        let sync_url = env::var("TURSO_SYNC_URL").unwrap();
+        let auth_token = env::var("TURSO_TOKEN").unwrap();
 
-    let db_path = env::var("DB_PATH").unwrap();
-    let sync_url = env::var("TURSO_SYNC_URL").unwrap();
-    let auth_token = env::var("TURSO_TOKEN").unwrap();
+        println!(
+            "Here are the env vars: db_path: {:?}, auth_token: {:?}, _syncrl: {:?}",
+            db_path, auth_token, sync_url
+        );
 
-    println!(
-        "Here are the env vars: db_path: {:?}, auth_token: {:?}, _syncrl: {:?}",
-        db_path, auth_token, sync_url
-    );
+        let db = Database::open_with_remote_sync(db_path, sync_url, auth_token).await?; 
 
-    let db = match Database::open_with_remote_sync(db_path, sync_url, auth_token).await {
-        Ok(db) => db,
-        Err(error) => {
-            eprintln!("Error connecting to remote sync server: {}", error);
-            return Err(error.to_string());
-        }
-    };
+        let conn = db.connect()?;
 
-    let conn = db.connect().unwrap();
+        let updated_at = match SystemTime::now().duration_since(UNIX_EPOCH) {
+            Ok(n) => n.as_secs(),
+            Err(_) => panic!("SystemTime before UNIX EPOCH!"),
+        };
 
-    let updated_at = match SystemTime::now().duration_since(UNIX_EPOCH) {
-        Ok(n) => n.as_secs(),
-        Err(_) => panic!("SystemTime before UNIX EPOCH!"),
-    };
+        let params = params!(new_text, updated_at, id.clone());
 
-    let params = params!([new_text.to_string(), updated_at.to_string(), id.to_string()]);
+        conn.query(
+            "UPDATE notes SET text = ?, updated_at = ? WHERE id = ?",
+            params,
+        )
+            .await?;
 
-    conn.query(
-        "UPDATE notes SET text = ?, updated_at = ? WHERE id = ?",
-        params,
-    )
-    .await
-    .unwrap();
+        let mut results = conn
+            .query("SELECT * from notes WHERE id = ?", params![id.to_string()])
+            .await?;
 
-    let results = conn
-        .query("SELECT * from notes WHERE id = ?", params![id.to_string()])
-        .await
-        .unwrap();
-
-    let mut updated_note;
-    while let Some(row) = results.next().unwrap() {
+        let row = results.next()?.unwrap();
         let updated_note: NoteItem = NoteItem {
             id: row.get(0).unwrap(),
             title: row.get(1).unwrap(),
@@ -201,19 +184,19 @@ async fn update_note(id: String, new_text: String) -> Result<NoteItem, String> {
             updated_at: row.get(4).unwrap(),
         };
         info!("  {:?}", updated_note);
-        break;
-    }
 
-    print!("Syncing with remote database...");
-    db.sync().await.unwrap();
-    println!(" done");
+        print!("Syncing with remote database...");
+        db.sync().await?;
+        println!(" done");
 
-    format!("Updated note: {:?}", updated_note);
+        format!("Updated note: {updated_note:?}");
 
-    Ok(updated_note)
+        Ok(updated_note)
 }
 
 fn main() {
+    tracing_subscriber::fmt::init();
+
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![
             get_all_notes,
