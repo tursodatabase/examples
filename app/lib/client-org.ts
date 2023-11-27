865f2faad5400d55ca046675e8de0be5547cdc9f
James Sinkala <xinnks@gmail.com>
2023-11-27 06:01:56 +0300

    feat: Add new Turso client constructors

--- app/lib/client-org.ts
+++ app/lib/client-org.ts
@@ -1,6 +1,8 @@
 import { drizzle } from "drizzle-orm/libsql";
 import { createClient } from "@libsql/client/http";
 import * as schema from "../../drizzle/org-schema";
+import Database from "libsql";
+import { tenantDbLocalPath } from "./utils";
 
 interface Env {
   url: string;
@@ -19,5 +21,21 @@ export function buildDbClient({ url }: Env) {
 
   return drizzle(createClient({ url: `libsql://${url}`, authToken }), {
     schema,
+  });
+}
+
+export function _buildOrgDbClient({ url }: Env) {
+  if (url === undefined || url == null) {
+    throw new Error("db url is not defined");
+  }
+
+  const authToken = (process.env as unknown as Env).TURSO_DB_AUTH_TOKEN?.trim();
+  if (authToken === undefined) {
+    throw new Error("TURSO_DB_AUTH_TOKEN is not defined");
+  }
+
+  return new Database(tenantDbLocalPath(url), {
+    syncUrl: `libsql://${url}`,
+    authToken,
   });
 }
--- app/lib/client.ts
+++ app/lib/client.ts
@@ -1,3 +1,5 @@
+// @ts-ignore
+import Database from "libsql";
 import { drizzle } from "drizzle-orm/libsql";
 import { createClient } from "@libsql/client/http";
 import * as schema from "../../drizzle/schema";
@@ -5,6 +7,7 @@ import * as schema from "../../drizzle/schema";
 interface Env {
   TURSO_DB_AUTH_TOKEN?: string;
   TURSO_DB_URL?: string;
+  APP_LOCAL_DB_PATH?: string;
 }
 
 export function buildDbClient() {
@@ -19,4 +22,28 @@ export function buildDbClient() {
   }
 
   return drizzle(createClient({ url, authToken }), { schema });
+}
+
+export function _buildServiceDbClient() {
+  const url = (process.env as unknown as Env).TURSO_DB_URL?.trim();
+  if (url === undefined) {
+    throw new Error("TURSO_DB_URL is not defined");
+  }
+
+  const authToken = (process.env as unknown as Env).TURSO_DB_AUTH_TOKEN?.trim();
+  if (authToken === undefined) {
+    throw new Error("TURSO_DB_AUTH_TOKEN is not defined");
+  }
+
+  const localDbPath = (process.env as unknown as Env).APP_LOCAL_DB_PATH?.trim();
+  if (localDbPath === undefined) {
+    throw new Error("APP_LOCAL_DB_PATH is not defined");
+  }
+
+  const options = {
+    syncUrl: process.env.TURSO_DB_URL,
+    authToken: process.env.TURSO_DB_AUTH_TOKEN,
+  };
+
+  return new Database(localDbPath, options);
 }
