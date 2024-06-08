6d1e659eea0f97f1a169128ebb8a150b74022046
Imam Ali Mustofa <d.darkterminal@gmail.com>
2024-06-08 12:17:39 +0700

    docs: create readme for laravel todo list

--- laravel-authenticated-todo-list/database/seeders/DatabaseSeeder.php
+++ laravel-authenticated-todo-list/database/seeders/DatabaseSeeder.php
@@ -3,7 +3,6 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,10 +13,5 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         User::factory(10)->create();
-
-        // User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
     }
 }
